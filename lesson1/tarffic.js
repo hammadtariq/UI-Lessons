502f30bebfb30e4f6218f06af38f23b74f88a3a8
Muhammad Ahmed <muhahmed@nisum.com>
2020-01-01 04:11:26 -0800

    Updated Traffic Signal

--- lesson1/tarffic.js
+++ lesson1/tarffic.js
@@ -1,25 +1,42 @@
+  
+  
   var myvar = document.getElementsByClassName("dot");
-  color = ["red", "blue", "green", "black"];
-  //    var i=0;
-  let q;
+  var i=q=0;
+  color = ["red", "blue", "green", "white"];
+  function run(){
+      document.getElementById("b").disabled=false;
+    if (document.getElementById("a").innerHTML=="stop"){
+      document.getElementById("b").disabled=true;
+      
+      document.getElementById("a").innerHTML="start";
+      clearTimeout(q);
+      i = 0;
+     
+
+    }else{
+    StartSignal();
+    document.getElementById("a").innerHTML="stop";
+  }
+  }
   function StartSignal() {
+    
     myvar[i].style.backgroundColor = color[i];
-    q = setTimeout(myfunc2, 2000, i);
-    q = setTimeout(StartSignal, 2000, i);
-    console.log("sw34333dsd", q);
+    setTimeout(myfunc2, 2000, i,myvar,color);
 
     if (i == 2) {
       i = 0;
     } else {
       i++;
+      
     }
+    q = setTimeout(StartSignal, 2000,myvar,color,i,q);
+
   }
-  function myfunc2(i) {
+  function myfunc2(i,myvar,color) {
     myvar[i].style.backgroundColor = color[3];
   }
-  function restartStartSignal(abc) {
-    console.log("sdsd", abc);
+  function restartStartSignal() {
     clearTimeout(q);
     i = 0;
-    q = setTimeout(StartSignal, 2000);
+    setTimeout(StartSignal, 2000);
   }
