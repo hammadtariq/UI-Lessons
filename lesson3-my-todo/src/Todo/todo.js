2676ac2e6b849bedc789549b9850659fae38dd5a
Arsalan Ahmed Nasir <anasir@nisum.com>
2020-01-07 19:16:50 +0500

    MyTodo Complete

--- lesson3-my-todo/src/Todo/todo.js
+++ lesson3-my-todo/src/Todo/todo.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
-//import "./todo.css";
 import "antd/dist/antd.css";
 import { Form, Icon, Input, Button, List } from "antd";
 
 class Todo extends Component {
   state = {
     val: "",
-    val2: "",
-    list: ["Abc", "DEF", "abc", "xyz", "CDe"],
+    val3: "",
+    list: [
+      { text: "Abc", flag: false },
+      { text: "DEF", flag: false },
+      { text: "abc", flag: false },
+      { text: "xyz", flag: false },
+      { text: "CDe", flag: false }
+    ],
     showText: false
+  };
+
+  func2 = e => {
+    this.setState({ val3: e.target.value });
   };
   func = e => {
     this.setState({ val2: e.target.value });
@@ -20,9 +29,9 @@ class Todo extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    let value = this.state.val;
+    let text = this.state.val;
     if (this.state.val !== "") {
-      let list = [...this.state.list, value];
+      let list = [...this.state.list, { text, flag: false }];
       this.setState({ list, val: "" });
     }
   };
@@ -37,89 +46,95 @@ class Todo extends Component {
 
   update = id => {
     const list = [...this.state.list];
-    list[id] = this.state.val2;
+    list[id].text = this.state.val2;
     this.setState({
       list,
       showText: !this.state.showText
     });
   };
 
-  updateText=id=>{
-    this.setState({ showText: !this.state.showText})
-    
-  }
+  updateText = id => {
+    const list = this.state.list;
+    if (list[id].flag === true) {
+      list[id].text = this.state.val3;
+      list[id].flag = false;
+      this.setState({ list, val3: "" });
+    } else {
+      list[id].flag = true;
+      this.setState({ list,val3:list[id].text });
+    }
+  };
 
   render() {
     return (
       <div className="mytodo">
-        <Form layout="horizontal" onSubmit={this.handleSubmit}>
+        <Form layout="horizontal" className="antd" onSubmit={this.handleSubmit}>
           <Form.Item>
             <Input
               prefix={
                 <Icon type="info-circle" style={{ color: "rgba(0,0,0,.25)" }} />
               }
-              placeholder="Add Task"
               onChange={this.handleChange}
               style={{ height: 60 }}
             />
-          </Form.Item>
 
+            <Button icon="plus" type="primary" block htmlType="submit" />
+          </Form.Item>
           <List
-            size="small"
+            size="large"
             bordered
-            dataSource={this.state.list.map((item, index) => (
-              <li key={index}>
-                {!this.state.showText ? (
-                  <span>
-                    <b>{item.toUpperCase()}</b>
-                    <Button
-                      type="danger"
-                      onClick={() => {
-                        this.delete(index);
-                      }}
-                    >
-                      X
-                    </Button>
-                    <Button
-                      type="primary"
-                      onClick={() => {this.updateText(index)}}
-                    >
-                      Update
-                    </Button>
-                  </span>
-                ) : (
-                  <span>
+            dataSource={this.state.list}
+            renderItem={(item, index) => {
+              return (
+                <List.Item>
+                  {item.flag ? (
                     <Form.Item>
                       <Input
+                        type="text"
+                        value={this.state.val3}
                         prefix={
                           <Icon
                             type="info-circle"
                             style={{ color: "rgba(0,0,0,.25)" }}
                           />
                         }
-                        placeholder="Add Task"
-                        onChange={this.func}
+                        onChange={this.func2}
                       />
+                      <Button
+                        type="primary"
+                        onClick={() => {
+                          this.updateText(index);
+                        }}
+                      >
+                        Update
+                      </Button>
                     </Form.Item>
-                    <Button
-                      type="primary"
-                      onClick={() => {
-                        this.update(index);
-                      }}
-                    >
-                      Update
-                    </Button>
-                  </span>
-                )}
-                
-              </li>
-            ))}
-            renderItem={item => <List.Item>{item}</List.Item>}
+                  ) : (
+                    <div className="my-item">
+                      <span>{item.text}</span>
+                      <Button
+                        type="danger"
+                        onClick={() => {
+                          this.delete(index);
+                        }}
+                      >
+                        X
+                      </Button>
+                      <Button
+                        type="primary"
+                        onClick={() => {
+                          this.updateText(index);
+                        }}
+                      >
+                        Update
+                      </Button>
+                    </div>
+                  )}
+                </List.Item>
+              );
+            }}
           />
-          <Form.Item>
-            <Button icon="plus" type="primary" block htmlType="submit" />
-          </Form.Item>
-        </Form>
+        </Form>{" "}
       </div>
     );
   }
