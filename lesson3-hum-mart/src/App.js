706698cec7f801e2f211d886285ec5508a4fedd7
Rehan Rabbani <rrabbani@nisum.com>
2020-01-08 20:28:37 +0500

    starting hum mart user interface

---
+++ lesson3-hum-mart/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+import Home  from  './components/Home'
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
