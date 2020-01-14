ff6aeff661acbe48a5bb29f368ebbb8d286afee6
Muhammad Ahmed <ahmed.tahir000000@gmail.com>
2020-01-14 06:44:02 -0800

    Cart functionality Added

--- lesson 03/src/App.js
+++ lesson 03/src/App.js
@@ -1,51 +1,35 @@
-import React from 'react'
-import './App.css'
-import Header from './Header'
-import LoginHeader from './LoginHeader'
-import Logodiv from './Logodiv'
-import Category from './Category'
-import Maindiv from './Maindiv'
-import Footer from './Footer'
-import Mobilediv from './Mobilediv'
-import Mybody from './Body'
-import Content from './Content'
-import Home from './Home'
+import React ,{Component}from "react";
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
-
-
-
-
-class App extends React.Component{
- 
-
-  
-  render(){
-    return(
-      <div>
-      {/* <Header/>
-      <LoginHeader/>
-      <Logodiv/>
-      <Category/>
-      <Maindiv/>
-      <Mybody/>
-      <Content/>
-      <Mobilediv/>
-      <Footer/> */}
-      <Home/>
-      </div>
-    )
-      
-    
+import "./App.css";
+import Header from "./Header";
+import LoginHeader from "./LoginHeader";
+import Logodiv from "./Logodiv";
+import Category from "./Category";
+import Maindiv from "./Maindiv";
+import Footer from "./Footer";
+import Mobilediv from "./Mobilediv";
+import Mybody from "./Body";
+import Content from "./Content";
+import Home from "./Home";
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+        
+          {/* <Home/> */}
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path='/Header' component={Header} />
+            <Route path='/Footer' component={Footer} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
-
-
-
-
-
-
-
-
 
 export default App;
